b7511fcc2eecdc0d6b922148f0e4d793b9250444
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-03 20:29:23 -0300

    requisito-8

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('hi');
+const paletaDeCor = document.getElementById('color-palette');
+const painel = document.getElementsByTagName('ul')[0];
+let corSelecionada;
+let divSelecionada = document.getElementsByClassName('selected')[0];
+
+function selecionaCor(event) {
+  paletaDeCor.addEventListener('click', function (event) {
+    if (event.target.classList[0] == 'color') {
+      divSelecionada.classList.remove('selected');
+      event.target.classList.add('selected');
+      divSelecionada = event.target;
+    }
+  });
+}
+selecionaCor();
+
+function pintarQuadro() {
+  painel.addEventListener('click', function (event) {
+    if (event.target.classList == 'pixel') {
+      event.target.style.backgroundColor = divSelecionada.classList[1];
+    }
+  });
+}
+pintarQuadro();
