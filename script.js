642c3675b5ffc454ca80d55000586bc1dfa770c2
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-04 09:26:30 -0300

    requisito-9

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const paletaDeCor = document.getElementById('color-palette');
 const painel = document.getElementsByTagName('ul')[0];
+const button = document.getElementById('clear-board');
+const pixel =  document.getElementsByClassName('pixel');
 let corSelecionada;
 let divSelecionada = document.getElementsByClassName('selected')[0];
 
@@ -16,9 +18,18 @@ selecionaCor();
 
 function pintarQuadro() {
   painel.addEventListener('click', function (event) {
-    if (event.target.classList == 'pixel') {
-      event.target.style.backgroundColor = divSelecionada.classList[1];
+    if (event.target.classList[0] == 'pixel') {
+      event.target.classList.add(divSelecionada.classList[1]);
     }
   });
 }
 pintarQuadro();
+
+function clear() {
+  button.addEventListener('click', function () {
+    for (let i = 0; i < pixel.length; i += 1) {
+      pixel[i].classList = 'pixel white';
+    }
+  });
+}
+clear();
