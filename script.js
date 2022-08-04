d0d2720b400eb2b99b982730d8750b121289191d
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-04 19:17:58 -0300

    requisito-10

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const paletaDeCor = document.getElementById('color-palette');
 const painel = document.getElementsByTagName('ul')[0];
-const button = document.getElementById('clear-board');
+const buttonClear = document.getElementById('clear-board');
 const pixel =  document.getElementsByClassName('pixel');
+const inputNumber = document.getElementById('board-size');
+const buttonGenerate = document.getElementById('generate-board');
 let corSelecionada;
 let divSelecionada = document.getElementsByClassName('selected')[0];
 
@@ -32,10 +34,45 @@ function pintarQuadro() {
 pintarQuadro();
 
 function clear() {
-  button.addEventListener('click', function () {
+  buttonClear.addEventListener('click', function () {
     for (let i = 0; i < pixel.length; i += 1) {
       pixel[i].className = 'pixel white';
     }
   });
 }
 clear();
+
+function generateBoard() {
+  buttonGenerate.addEventListener('click', function () {
+    if (inputNumber.value > 0) {
+      let li = document.getElementsByTagName('li');
+      if (li.length > 0) {
+        removeBoard();
+      }
+      for (let i = 0; i < inputNumber.value; i+= 1) {
+        let novaLi = document.createElement('li');
+        painel.appendChild(novaLi);
+        let lista = document.getElementsByTagName('li')[i];
+        for (let n = 0; n < inputNumber.value; n+= 1) {
+          let novaDiv = document.createElement('div');
+          novaDiv.className = 'pixel white';
+          lista.appendChild(novaDiv);
+        }
+      }
+    } else {
+      let li = document.getElementsByTagName('li');
+      if (li.value == '' || li.value == null) {
+        window.alert('Board inválido!')
+      }
+    }
+  });
+}
+generateBoard();
+
+function removeBoard() {
+  while (painel.firstChild) {
+    painel.removeChild(painel.firstChild);
+  }
+}
+
+
